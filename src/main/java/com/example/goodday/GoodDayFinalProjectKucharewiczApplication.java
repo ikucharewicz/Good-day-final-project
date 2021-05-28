7a8322adb9066f3c13af10679469f822eeb3930a
iwona <ikucharewicz@gmail.com>
2021-05-28 12:53:57 +0200

    initialization of the project (Spring Boot)

---
+++ src/main/java/com/example/goodday/GoodDayFinalProjectKucharewiczApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodDayFinalProjectKucharewiczApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodDayFinalProjectKucharewiczApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goodday/GoodDayFinalProjectKucharewiczApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodDayFinalProjectKucharewiczApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
